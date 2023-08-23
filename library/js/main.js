a861ee5612ea8794784d5062a415d6a414a7d5e8
Julia Voronovich <Y-1306@mail.ru>
2023-08-23 17:55:16 +0300

    feat: add menu auth and profile to localStorage

--- library/js/main.js
+++ library/js/main.js
@@ -1,14 +1,40 @@
+let activeProfile = '';
+let activeCardNumber = '';
+let activeProfileMenu = '.drop-menu-noauth';
+const ACTIVE = 'active';
+
+let visited = 0;
+let books = 0;
+
+let _isClickProfile = false;
+
+window.onload= function(event) {
+
+    activeProfile = getActiveProfile ();
+    console.log(activeProfile);
+    if (activeProfile) {
+        showProfileIcon(activeProfile.name, activeProfile.surname);
+        showProfileMenu (activeCardNumber);
+    }   
+};   
+
+function getActiveProfile () {
+    activeCardNumber = localStorage.getItem(ACTIVE);
+    activeProfile = localStorage.getItem(activeCardNumber);
+    
+    return JSON.parse(activeProfile);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('burger').addEventListener('click', function() {    
 
         document.querySelector('.header').classList.toggle('open');
         document.querySelector('body').classList.toggle('body-overflow');
 
-        document.querySelector('.drop-menu-auth').classList.remove('open');
+        document.querySelector(activeProfileMenu).classList.remove('open');
         
     })
 });
-
 
 // Закрыть при клике вне меню
 document.getElementById('nav').addEventListener('click', event => {
@@ -42,22 +68,29 @@ function closeOnClick() {
 
  // открытие меню авторизации
  document.querySelector('.profile-icon').addEventListener('click', event => {
-    document.querySelector('.drop-menu-auth').classList.toggle('open');
+    document.querySelector(activeProfileMenu).classList.toggle('open');
 });
 
 document.querySelector('.profile-icon').addEventListener('click', event => {
     event._isClickProfile = true;
 });
-document.querySelector('.drop-menu-title').addEventListener('click', event => {
-    event._isClickProfile = true;
+const menuTitles = document.querySelectorAll('.drop-menu-title');
+menuTitles.forEach((menuTitle,) => {
+    menuTitle.addEventListener ('click', event => {
+        event._isClickProfile = true;
+    });
 });
-document.querySelector('.drop-menu-list').addEventListener('click', event => {
-    event._isClickProfile = true;
+const menuLists = document.querySelectorAll('.drop-menu-list');
+menuLists.forEach((menuList) => {
+    menuList.addEventListener ('click', event => {
+        event._isClickProfile = true;
+    });
 });
 document.body.addEventListener('click', event => {
+    console.log(event._isClickProfile);
     if (event._isClickProfile) return;
 
-    document.querySelector('.drop-menu-auth').classList.remove('open');
+    document.querySelector(activeProfileMenu).classList.remove('open');
 });
 
 
@@ -70,3 +103,109 @@ document.querySelector('#button-check-card').addEventListener('click', event => 
     return false;
  
 });
+
+//форма Register
+const formRegister = document.forms.register;
+const EMAIL_REGEXP = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+function validation (form) {  
+
+    const elementsForm = form.querySelectorAll('input');
+    let result = true;   
+
+    for (const element of elementsForm) {
+
+        removeError(element);
+
+        if (element.value === '') {
+            text = 'fill in the empty field';
+            createError(element, text);
+            result = false;
+        } else {
+            if (element.name === 'password' && element.value.length < 8)  {
+                text = 'password less than 8 characters';
+                createError(element, text);
+                result = false;
+            }
+            if (element.name === 'email' && !EMAIL_REGEXP.test(element.value)) {
+                text = 'not correct email';
+                createError(element, text);
+                result = false;
+            }
+         }
+    }    
+    return result;
+
+    function createError(element, text) {
+
+        const errorSpan = document.createElement('span');
+        errorSpan.classList.add('error');
+        errorSpan.textContent = text;
+        element.after(errorSpan);        
+    }
+
+    function removeError(element) {
+
+        const el = element.nextSibling;           
+         if (el.classList) {
+            if (el.classList.contains('error')) {
+                el.remove();
+         }   
+        }
+    }
+}
+
+formRegister.addEventListener('submit', event => {
+
+    event.preventDefault();
+
+    if (validation(event.target)) {
+        //сабмит формы в localstorage
+        saveToLocalStorage(formRegister); 
+    }
+});
+
+function saveToLocalStorage (form) {
+
+    const formData = new FormData(form);
+    // теперь можно извлечь данные
+    const name = formData.get('name');
+    const surname = formData.get('surname');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const cardName = [...Array(9)].map(() => Math.floor(Math.random() * 16).toString(16)).join('').toUpperCase();
+
+    const profile = {"name":name, "surname":surname, "email":email, "password":password, "visited": 1, "books":0, "bonuses": 1250};
+
+    localStorage.setItem(cardName, JSON.stringify(profile));
+    localStorage.setItem('active', cardName);
+
+    showProfileIcon(name, surname);
+    closePopUp('.modal-login-wrap');
+
+    showProfileMenu(cardName);
+       
+}
+
+function  showProfileIcon(name, surname) {
+
+    const nameFirstLetter = name[0].toUpperCase();
+    const surnameFirstLetter = surname[0].toUpperCase();
+
+    const icon = document.querySelector('.profile-icon');
+    const tooltip = document.querySelector('.tooltiptext');
+    icon.innerHTML = `<svg height="28" width="28">
+    <circle cx="14" cy="14" r="50%" fill="white"></circle>
+    <text x="4" y="20" fill="#BB945F" font-size="15px">${nameFirstLetter}${surnameFirstLetter}</text>
+    </svg>`;
+    icon.title = name +' '+surname;  
+
+}
+function  showProfileMenu (cardName) {
+
+    document.querySelector(activeProfileMenu).classList.remove('open');
+    activeProfileMenu = '.drop-menu-auth';
+
+    document.querySelector('.menu-profile').innerHTML = cardName;
+    document.querySelector('.menu-profile').style.fontsize = '10px';
+}
--- library/js/popup.js
+++ library/js/popup.js
@@ -1,26 +1,23 @@
 // открытие поп-ап register
 const registers = document.querySelectorAll('.register');
 const body = document.querySelector('body');
-let unlock = true;
 
+let unlock = true;
 
 registers.forEach((register, i) => {
     
     register.addEventListener('click', event => {   
         openPopUp('.modal-login-wrap');        
-        document.querySelector('.drop-menu-auth').classList.remove('open');        
+        document.querySelector('.drop-menu-noauth').classList.remove('open');        
         
-    });
-    
+    });    
 });
-
 
 // закрытие поп-ап register
 document.querySelector('#close-register').addEventListener('click', event => {
     closePopUp('.modal-login-wrap');  
     event.preventDefault();  
 });
-
 
 function openPopUp(popUp) {
 
@@ -34,8 +31,7 @@ function openPopUp(popUp) {
         document.querySelector(popUp).classList.add('open');
 
         document.querySelector('.modal-login-wrap').addEventListener('click', event => {
-            console.log(event.target.closest('.modal-register'));
-            if (!event.target.closest('.modal-register') && event.target.closest('.modal-register') !== null) {
+              if (!event.target.closest('.modal-register') && event.target.closest('.modal-register') !== null) {
                 closePopUp('.modal-login-wrap');
             }
         });          
@@ -48,8 +44,7 @@ function closePopUp(popUp, doUnlock = true) {
         if(doUnlock) {
             bodyUnlock();
         }
-    }
-    
+    }    
 }
 
 function  bodyLock() {
@@ -64,63 +59,3 @@ function  bodyUnlock() {
 
 
 
-//форма Register
-const formRegister = document.forms.register;
-const EMAIL_REGEXP = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-
-function validation (form) {  
-
-    const elementsForm = form.querySelectorAll('input');
-    let result = true;
-
-    function createError(element, text) {
-
-        const errorSpan = document.createElement('span');
-        errorSpan.classList.add('error');
-        errorSpan.textContent = text;
-        element.after(errorSpan);        
-    }
-
-    function removeError(element) {
-
-        const el = element.nextSibling;           
-         if (el.classList) {
-            if (el.classList.contains('error')) {
-                el.remove();
-         }   
-        }
-    }
-
-    for (const element of elementsForm) {
-
-        removeError(element);
-
-        if (element.value === '') {
-            text = 'fill in the empty field';
-            createError(element, text);
-            result = false;
-        } else {
-            if (element.name === 'password' && element.value.length < 8)  {
-                text = 'password less than 8 characters';
-                createError(element, text);
-                result = false;
-            }
-            if (element.name === 'email' && !EMAIL_REGEXP.test(element.value)) {
-                text = 'not correct email';
-                createError(element, text);
-                result = false;
-            }
-         }
-    }    
-    return result;
-}
-
-formRegister.addEventListener('submit', event => {
-
-    event.preventDefault();
-
-    if (validation(event.target)) {
-        //сабмит формы в localstorage
-        console.log('ok');  
-    }
-});
