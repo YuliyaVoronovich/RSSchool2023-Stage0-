07ec2e1e6872bb3dc486ab57d1642a9527562757
Julia Voronovich <Y-1306@mail.ru>
2023-09-13 19:41:50 +0300

    feat: add next/prev buttons

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -1,14 +1,16 @@
 import songs from "./music.json" assert { type: "json" };
 
 const playButton = document.querySelector('.button-play');
+const nextButton = document.querySelector('.button-next');
+const prevButton = document.querySelector('.button-prev');
 
 const audio = new Audio();
 let isPlay = false;
+let currentIndex = 0;
 
-function playAudio() {
+function playAudio() {    
 
-    const currentSong = songs[0];
-
+    const currentSong = songs[currentIndex];
     if (!isPlay) {
         isPlay = true;
         audio.src = `../assets/audio/${currentSong.src}.mp3`;
@@ -28,4 +30,18 @@ function pauseAudio() {
     playButton.innerText = "play_arrow";
 }
 
+function nextAudio() {
+    isPlay = false;
+    currentIndex +=1;
+    playAudio();
+}
+
+function prevAudio() {
+    isPlay = false;
+    currentIndex -=1;
+    playAudio();
+}
+
 playButton.addEventListener('click', playAudio);
+nextButton.addEventListener('click', nextAudio);
+prevButton.addEventListener('click', prevAudio);
