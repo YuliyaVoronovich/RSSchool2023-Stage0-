40c55ba10f293cc7a75f6ae5b01a1c05b6309943
Julia Voronovich <Y-1306@mail.ru>
2023-09-13 20:00:31 +0300

    feat: add change img, name and artist

--- audio-player/js/index.js
+++ audio-player/js/index.js
@@ -4,6 +4,12 @@ const playButton = document.querySelector('.button-play');
 const nextButton = document.querySelector('.button-next');
 const prevButton = document.querySelector('.button-prev');
 
+const audioImg = document.querySelector('.audio-img');
+const blur = document.querySelector('.blur');
+
+const nameSong = document.querySelector('.name-song');
+const artistSong = document.querySelector('.artist-song');
+
 const audio = new Audio();
 let isPlay = false;
 let currentIndex = 0;
@@ -16,7 +22,11 @@ function playAudio() {
         audio.src = `../assets/audio/${currentSong.src}.mp3`;
         audio.currentTime = 0;
         audio.play();  
-        playButton.innerText = "pause";    
+        playButton.innerText = "pause";  
+        audioImg.src =  `../assets/img/${currentSong.img}.jpg`;
+        blur.style.backgroundImage=`url(../assets/img/${currentSong.img}.jpg)`;
+        nameSong.innerText = `${currentSong.name}`;
+        artistSong.innerText = `${currentSong.artist}`;
 
     } else {
         pauseAudio();
