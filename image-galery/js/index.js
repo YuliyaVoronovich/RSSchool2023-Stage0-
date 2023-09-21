ceae315d0877246a5a73af9322b0925a321c5c3e
Julia Voronovich <Y-1306@mail.ru>
2023-09-21 23:25:58 +0300

    feat: search photos on username to click

--- image-galery/js/index.js
+++ image-galery/js/index.js
@@ -3,6 +3,8 @@ const KEY = 'yaQ7ydsJ2lF3tYD6evKdJNpjw8RLGInYmnqO2_IMje8';
 
 const gallery = document.querySelector(".gallery-list");
 const form = document.querySelector(".form-search");
+const ownName = document.querySelector(".own-name");
+let searchInput = document.querySelector(".input-search");
 
 let queryDefault = 'random';
 const page = 0;
@@ -12,16 +14,24 @@ async function getData(query = queryDefault) {
     const response = await fetch(url);
     const data = await response.json();
    
-    showData(data);
+    showData(data.results);
 }
 
+async function getDataCollection(username, name) {
+    const url1 = `https://api.unsplash.com/users/${username}/photos?page=${page}&per_page=50&orientation=landscape&client_id=${KEY}`;
+    searchInput.value = name;
+    const response = await fetch(url1);
+    const data = await response.json();
+   
+    showData(data);
+} 
 function showData(data) {
     let picture = '';
-    data.results.map((element) => {
+    data.map((element) => {
          picture += `<li class="gallery-item" data-picture="${element.urls.full}">
           <img src="${element.urls.regular}" alt="${element.alt_description}" loading="lazy">
             <div class="image-info">
-                <div class="own-name">${element.user.name}</div>
+                <div class="own-name" onclick="getDataCollection('${element.user.username}', '${element.user.name}')">${element.user.name}</div>
                 <div class="own-likes">
                     <span class="material-symbols-outlined icon-likes">favorite</span>${element.likes}
                 </div>
@@ -42,5 +52,6 @@ form.addEventListener('submit', event => {
 
     getData(query);
 });
+
 
 getData();
